7f9552c1b992b83bd2cd97212deb77e2d93f8e29
Brieuc Fresnel <brieuc@studio-dot.fr>
2023-06-22 14:50:40 +0200

    feat: go back to static mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: vercel(),
+
 });
